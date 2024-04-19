702b01d3e6f380e809c22764d4503794277d15f4
Benny Reznik <bennyr@spiralsolutions.com>
2024-04-19 22:12:58 +0300

    First commit - running todo list application with minimal api#

---
+++ TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApi_Example_CSharp
+{
+    public class TodoDb: DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
